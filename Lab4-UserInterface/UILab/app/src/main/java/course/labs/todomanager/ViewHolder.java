1cacf935291b0adfdcaae93969ebfb4ceb7f54f5
johnshumon <johnshumon@gmail.com>
2014-10-27 14:12:34 +0200

    Lab-4 UI solutions

---
+++ Lab4-UserInterface/UILab/app/src/main/java/course/labs/todomanager/ViewHolder.java
@@ -0,0 +1,15 @@
+package course.labs.todomanager;
+
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+/**
+ * Created by kutimuti on 10/23/14.
+ */
+public class ViewHolder {
+
+    TextView title;
+    CheckBox status;
+    TextView priority;
+    TextView date;
+}
